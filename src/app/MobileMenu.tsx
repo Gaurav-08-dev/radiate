4a00f80bf317d729fbc9b8c645c1c86ffdae0842
gaurav <g8111997@gmail.com>
2025-03-28 00:32:28 +0530

    updates

--- src/app/MobileMenu.tsx
+++ src/app/MobileMenu.tsx
@@ -121,8 +121,7 @@ export function MobileMenu({ collections, featuredProducts }: MobileMenuProps) {
     if (isOpen && sheetContentRef.current) {
       sheetContentRef.current.focus();
     }
-  }, [isOpen,sheetContentRef.current]);
-
+  }, [isOpen, sheetContentRef.current]);
 
   return (
     <>
@@ -185,7 +184,7 @@ export function MobileMenu({ collections, featuredProducts }: MobileMenuProps) {
               </svg>
             </div>
           </SheetHeader>
-          <div className="scrollbar-hide mt-4 flex max-h-[90%] flex-col items-start space-y-5 overflow-y-auto [&::-webkit-scrollbar]:hidden">
+          <div className="scrollbar-hide mt-4 flex max-h-[90%] flex-col items-start space-y-3 overflow-y-auto [&::-webkit-scrollbar]:hidden">
             {searchQuery.trim() ? (
               <div className="w-full">
                 {isLoading ? (
@@ -239,7 +238,6 @@ export function MobileMenu({ collections, featuredProducts }: MobileMenuProps) {
                                             href={`/products/${product?.slug}`}
                                             className={`${playfair.className} line-clamp-2 text-xs font-medium`}
                                             onClick={resetSearch}
-                                            
                                           >
                                             {product?.name}
                                           </Link>
@@ -256,7 +254,9 @@ export function MobileMenu({ collections, featuredProducts }: MobileMenuProps) {
                                           />
                                         </div>
                                         <div className="text-xs font-medium">
-                                          {product?.priceData?.formatted?.discountedPrice || product?.price?.formatted?.price}
+                                          {product?.priceData?.formatted
+                                            ?.discountedPrice ||
+                                            product?.price?.formatted?.price}
                                         </div>
                                         <AddToCartButton
                                           variant="default"
@@ -277,7 +277,7 @@ export function MobileMenu({ collections, featuredProducts }: MobileMenuProps) {
                               </div>
                               <Link
                                 href="/shop"
-                                className="w-fit p-2 text-sm mt-4 ml-[27%] border border-purple-600 font-medium text-purple-600 hover:text-purple-800"
+                                className="ml-[27%] mt-4 w-fit border border-purple-600 p-2 text-sm font-medium text-purple-600 hover:text-purple-800"
                                 onClick={resetSearch}
                               >
                                 Explore all products
@@ -317,7 +317,9 @@ export function MobileMenu({ collections, featuredProducts }: MobileMenuProps) {
                                   </div> */}
                                 <div className="flex flex-col gap-2">
                                   <div className="font-medium">
-                                    {result.priceData?.formatted?.discountedPrice || result.price?.formatted?.price}
+                                    {result.priceData?.formatted
+                                      ?.discountedPrice ||
+                                      result.price?.formatted?.price}
                                   </div>
                                   <AddToCartButton
                                     // selectedOptions={selectedOptions}
@@ -360,13 +362,13 @@ export function MobileMenu({ collections, featuredProducts }: MobileMenuProps) {
                 type="multiple"
                 value={openAccordions}
                 onValueChange={setOpenAccordions}
-                className="w-full"
+                className="w-full space-y-3"
               >
                 <AccordionItem value="shop" className="border-b-0">
                   <AccordionTrigger
                     className={cn(
                       playfair.className,
-                      "pt-0 text-sm hover:no-underline",
+                      "py-0 text-sm hover:no-underline",
                     )}
                   >
                     Shop
@@ -433,10 +435,10 @@ export function MobileMenu({ collections, featuredProducts }: MobileMenuProps) {
                   </AccordionContent>
                 </AccordionItem>
 
-                <div className="py-0">
+                <div className="py-0 mt-0">
                   <Link
                     href="/shop?collection=2398b8e1-88a1-93c4-2323-9a74d09770f8"
-                    className={cn(playfair.className, "text-sm font-medium")}
+                    className={cn(playfair.className, "mt-0 text-sm font-medium")}
                   >
                     Best-sellers
                   </Link>
@@ -446,7 +448,7 @@ export function MobileMenu({ collections, featuredProducts }: MobileMenuProps) {
                   <AccordionTrigger
                     className={cn(
                       playfair.className,
-                      "text-sm hover:no-underline",
+                      "py-0 text-sm hover:no-underline",
                     )}
                   >
                     Shop by scent
@@ -487,63 +489,63 @@ export function MobileMenu({ collections, featuredProducts }: MobileMenuProps) {
                 </AccordionItem>
               </Accordion>
             )}
-          </div>
-          {!searchQuery.trim() && (
-            <div className="flex flex-col items-start space-y-3 text-sm font-medium">
-              <div
-                className={cn(
-                  playfair.className,
-                  "flex flex-col space-y-3 text-sm font-medium",
-                )}
-              >
-                <Link href="/faq">FAQs</Link>
-                <Link href="/return-refund-policy">
-                  Return & shipping policy
-                </Link>
-                <Link href="/about-us">About us</Link>
-                <Link href="#contact">Contact us</Link>
-              </div>
+            {!searchQuery.trim() && (
+              <div className="flex flex-col items-start space-y-3 text-sm font-medium">
+                <div
+                  className={cn(
+                    playfair.className,
+                    "flex flex-col space-y-3 text-sm font-medium",
+                  )}
+                >
+                  <Link href="/faq">FAQs</Link>
+                  <Link href="/return-refund-policy">
+                    Return & shipping policy
+                  </Link>
+                  <Link href="/about-us">About us</Link>
+                  <Link href="#contact">Contact us</Link>
+                </div>
 
-              <div className="flex gap-4 pt-4">
-                <Link
-                  href="https://www.instagram.com/letsradiate.in/#"
-                  className="text-gray-800 hover:text-gray-600"
-                >
-                  <svg
-                    className="h-6 w-6"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
+                <div className="flex gap-4 pt-4">
+                  <Link
+                    href="https://www.instagram.com/letsradiate.in/#"
+                    className="text-gray-800 hover:text-gray-600"
                   >
-                    <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
-                  </svg>
-                </Link>
-                <Link
-                  href="https://youtube.com/@letsradiate-in"
-                  className="text-gray-800 hover:text-gray-600"
-                >
-                  <svg
-                    className="h-6 w-6"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
+                    <svg
+                      className="h-6 w-6"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+                    </svg>
+                  </Link>
+                  <Link
+                    href="https://youtube.com/@letsradiate-in"
+                    className="text-gray-800 hover:text-gray-600"
                   >
-                    <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
-                  </svg>
-                </Link>
-                <Link
-                  href="https://facebook.com"
-                  className="text-gray-800 hover:text-gray-600"
-                >
-                  <svg
-                    className="h-6 w-6"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
+                    <svg
+                      className="h-6 w-6"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
+                    </svg>
+                  </Link>
+                  <Link
+                    href="https://facebook.com"
+                    className="text-gray-800 hover:text-gray-600"
                   >
-                    <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
-                  </svg>
-                </Link>
+                    <svg
+                      className="h-6 w-6"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
+                    </svg>
+                  </Link>
+                </div>
               </div>
-            </div>
-          )}
+            )}
+          </div>
         </SheetContent>
       </Sheet>
     </>
